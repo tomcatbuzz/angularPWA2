a64d3734bfbcb3e0053e7aa5f05d7114a4f81063
tomcatbuzz <tomcatbuzz@yahoo.com>
2019-06-15 16:21:29 -0400

    updates

---
+++ src/app/photo-share/photo-share.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-photo-share',
+  templateUrl: './photo-share.component.html',
+  styleUrls: ['./photo-share.component.scss']
+})
+export class PhotoShareComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/photo-share/photo-share.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PhotoShareComponent } from './photo-share.component';
+
+@NgModule({
+  declarations: [PhotoShareComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class PhotoShareModule { }
